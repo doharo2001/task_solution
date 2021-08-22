e2f5ca937726debe0e7641a9bb8aea594c1fa24f
Артемий <doharo2001@gmail.com>
2021-08-22 16:11:18 +0300

    task_solution
    Решение задач на языке программирование C#

---
+++ Массивы/Массивы/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Массивы
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[11];
+            int i = 0;
+            Random rnd = new Random();
+            int rndnumber;
+            for (i = 0; i < arr.Length; i++)
+            {
+                rndnumber = rnd.Next(-15, 15);
+                arr[i] = rndnumber;
+                Console.Write(arr[i] + "  ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
